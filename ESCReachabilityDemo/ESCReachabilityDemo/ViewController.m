a188402eaf2edabd14308ec1197efeb7f8bd884a
xiang <850794895@qq.com>
2019-04-28 09:40:13 +0800

    添加通知

--- ESCReachabilityDemo/ESCReachabilityDemo/ViewController.m
+++ ESCReachabilityDemo/ESCReachabilityDemo/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+@property(nonatomic,strong)Reachability* reachability;
+
 @end
 
 @implementation ViewController
@@ -34,7 +36,27 @@
             NSLog(@"使用WiFi网络");
             break;
     }
-    
+    self.reachability = r;
+    [self.reachability startNotifier];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(networkHasChanged:) name:kReachabilityChangedNotification object:nil];
+}
+
+- (void)networkHasChanged:(NSNotification *)notification {
+    NSLog(@"%@",notification);
+    switch ([notification.object currentReachabilityStatus]) {
+        case NotReachable:
+            // 没有网络连接
+            NSLog(@"没有网络连接");
+            break;
+        case ReachableViaWWAN:
+            // 使用3G网络
+            NSLog(@"使用3G网络");
+            break;
+        case ReachableViaWiFi:
+            // 使用WiFi网络
+            NSLog(@"使用WiFi网络");
+            break;
+    }
 }
 
 
